4f2556b146c5b25b52ec840bfb485b05993b8461
rsbbrs <rsb102.rs@gmail.com>
2022-08-31 17:25:05 -0400

    Extra stuff while trying to compile the damn thing!

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,9 @@
 add_rules("mode.debug", "mode.release")
 add_requires("glfw")
 
+set_policy("build.warning", true) -- show warnings
+set_warnings("all") -- warn about many things
+
 target("helloworld")
     set_kind("binary")
     set_languages("cxx17")
