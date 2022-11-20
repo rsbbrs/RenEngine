18c187c889118d9250a24b199b3c26665765d746
rsbbrs <rsb102.rs@gmail.com>
2022-11-19 20:38:28 -0500

    Setup for physics sim with two balls

--- assets/scripts/physDemoSetup.lua
+++ assets/scripts/physDemoSetup.lua
@@ -1,73 +1,69 @@
--- Loads the spaceship sprite. --
-if(loadImage("Spaceship", "sprites\\spaceship.png")) then
-    print("Successfully loaded spaceship.png")
-else
-    print("Failed to load spaceship.png")
-end
-
+-- Loads the ball sprite.
 if(loadImage("Ball", "sprites\\ball.png")) then
     print("Successfully loaded ball.png")
 else
     print("Failed to load ball.png")
 end
 
--- Creates the spaceship entity with initial parameters. --
-spaceship = createEntity()
+-- Ball 1 --
+ball1 = createEntity()
 
-getSprite(spaceship).name = "Spaceship"
+getSprite(ball1).name = "Ball 1"
 
-getPosition(spaceship).x = 0.0
-getPosition(spaceship).y = 0.0
-getPosition(spaceship).z = 1.0
+getPosition(ball1).x = -140.0
+getPosition(ball1).y = -50.0
+getPosition(ball1).z = 1.0
 
-getRigidBody(spaceship).velocity.x = 0.0
-getRigidBody(spaceship).velocity.y = 0.0
+getRigidBody(ball1).velocity.x = 0.0
+getRigidBody(ball1).velocity.y = 0.0
 
-getRigidBody(spaceship).acceleration.x = 0
-getRigidBody(spaceship).acceleration.y = 0
+getRigidBody(ball1).acceleration.x = 0
+getRigidBody(ball1).acceleration.y = 0
 
-getRigidBody(spaceship).gravity.x = 0.0
-getRigidBody(spaceship).gravity.y = 0.0
+getRigidBody(ball1).gravity.x = 0.0
+getRigidBody(ball1).gravity.y = 0.0
 
-getRigidBody(spaceship).force.x = 0.0
-getRigidBody(spaceship).force.y = 0.0
+getRigidBody(ball1).force.x = 0.0
+getRigidBody(ball1).force.y = 0.0
 
-getRigidBody(spaceship).mass = 100.0
-getRigidBody(spaceship).static = true
+getRigidBody(ball1).mass = 10.0
 
-getScale(spaceship).scale = 25
+getScale(ball1).scale = 10
 
--- Ball entity --
-ball = createEntity()
+-- Ball 2 --
+ball2 = createEntity()
 
-getSprite(ball).name = "Ball"
+getSprite(ball2).name = "Ball 2"
 
-getPosition(ball).x = -140.0
-getPosition(ball).y = -50.0
-getPosition(ball).z = 1.0
+getPosition(ball2).x = 140.0
+getPosition(ball2).y = -50.0
+getPosition(ball2).z = 1.0
 
-getRigidBody(ball).velocity.x = 0.0
-getRigidBody(ball).velocity.y = 0.0
+getRigidBody(ball2).velocity.x = 0.0
+getRigidBody(ball2).velocity.y = 0.0
 
-getRigidBody(ball).acceleration.x = 0
-getRigidBody(ball).acceleration.y = 0
+getRigidBody(ball2).acceleration.x = 0
+getRigidBody(ball2).acceleration.y = 0
 
-getRigidBody(ball).gravity.x = 0.0
-getRigidBody(ball).gravity.y = 0.0
+getRigidBody(ball2).gravity.x = 0.0
+getRigidBody(ball2).gravity.y = 0.0
 
-getRigidBody(ball).force.x = 0.0
-getRigidBody(ball).force.y = 0.0
+getRigidBody(ball2).force.x = 0.0
+getRigidBody(ball2).force.y = 0.0
 
-getRigidBody(ball).mass = 100.0
+getRigidBody(ball2).mass = 10.0
 
-getScale(ball).scale = 10
+getScale(ball2).scale = 10
 
 -- Loads and attaches the runtime script to the spaceship --
 filepath = "scripts\\physics.lua"
-if(loadScript("Spaceship", filepath, false)) then
+if(loadScript("physics", filepath, false)) then
     print("Successfully loaded physics.lua")
-    getScript(spaceship).name = "Spaceship"
-    getScript(spaceship).path = filePath(filepath)
+    getScript(ball1).name = "physics"
+    getScript(ball2).name = "physics"
+
+    getScript(ball1).path = filePath(filepath)
+    getScript(ball2).path = filePath(filepath)
 else
     print("Failed to load physics.lua")
 end
--- assets/scripts/physics.lua
+++ assets/scripts/physics.lua
@@ -5,31 +5,60 @@ end
 
 -- Initiates physics simulation --
 if(keyPressed(input_code.enter)) then
-    getRigidBody(ball).velocity.x = 150.0
-    getRigidBody(ball).velocity.y = 100.0
+    -- Ball 1 --
+    getRigidBody(ball1).velocity.x = 150.0
+    getRigidBody(ball1).velocity.y = 100.0
 
-    getRigidBody(ball).gravity.x = 0
-    getRigidBody(ball).gravity.y = -9.8
+    getRigidBody(ball1).gravity.x = 0
+    getRigidBody(ball1).gravity.y = -9.8
 
-    getRigidBody(ball).force.x = 200.0
-    getRigidBody(ball).force.y = 20.0
+    getRigidBody(ball1).force.x = 200.0
+    getRigidBody(ball1).force.y = 20.0
+
+    -- Ball 2 --
+    getRigidBody(ball2).velocity.x = -150.0
+    getRigidBody(ball2).velocity.y = 100.0
+
+    getRigidBody(ball2).gravity.x = 0
+    getRigidBody(ball2).gravity.y = -9.8
+
+    getRigidBody(ball2).force.x = -200.0
+    getRigidBody(ball2).force.y = 20.0
 end
 
 -- Resets the simulation --
 if(keyPressed(input_code.left_ctrl)) then
-    getPosition(ball).x = -140.0
-    getPosition(ball).y = -50.0
-    getPosition(ball).z = 1.0
+    -- Ball 1 --
+    getPosition(ball1).x = -140.0
+    getPosition(ball1).y = -50.0
+    getPosition(ball1).z = 1.0
 
-    getRigidBody(ball).velocity.x = 0.0
-    getRigidBody(ball).velocity.y = 0.0
+    getRigidBody(ball1).velocity.x = 0.0
+    getRigidBody(ball1).velocity.y = 0.0
     
-    getRigidBody(ball).acceleration.x = 0.0
-    getRigidBody(ball).acceleration.y = 0.0
+    getRigidBody(ball1).acceleration.x = 0.0
+    getRigidBody(ball1).acceleration.y = 0.0
 
-    getRigidBody(ball).gravity.x = 0.0
-    getRigidBody(ball).gravity.y = 0.0
+    getRigidBody(ball1).gravity.x = 0.0
+    getRigidBody(ball1).gravity.y = 0.0
 
-    getRigidBody(ball).force.x = 0.0
-    getRigidBody(ball).force.y = 0.0
+    getRigidBody(ball1).force.x = 0.0
+    getRigidBody(ball1).force.y = 0.0
+
+    -- Ball 2 --
+    getPosition(ball2).x = -140.0
+    getPosition(ball2).y = -50.0
+    getPosition(ball2).z = 1.0
+
+    getRigidBody(ball2).velocity.x = 0.0
+    getRigidBody(ball2).velocity.y = 0.0
+    
+    getRigidBody(ball2).acceleration.x = 0.0
+    getRigidBody(ball2).acceleration.y = 0.0
+
+    getRigidBody(ball2).gravity.x = 0.0
+    getRigidBody(ball2).gravity.y = 0.0
+
+    getRigidBody(ball2).force.x = 0.0
+    getRigidBody(ball2).force.y = 0.0
 end
